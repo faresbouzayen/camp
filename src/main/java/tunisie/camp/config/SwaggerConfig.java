4912f61343067df3a4e2d7d6fd7cb2d356b07654
BaslyGaith <77758765+BaslyGaith@users.noreply.github.com>
2023-06-16 11:30:33 +0100

    new add

--- src/main/java/tunisie/camp/config/SwaggerConfig.java
+++ src/main/java/tunisie/camp/config/SwaggerConfig.java
@@ -1,17 +1,27 @@
- 
- import io.swagger.v3.oas.models.OpenAPI; 
- import io.swagger.v3.oas.models.info.Contact; 
- import io.swagger.v3.oas.models.info.Info; 
- import org.springframework.context.annotation.Bean; 
- import org.springframework.context.annotation.Configuration; 
-  
- @Configuration 
- public class SwaggerConfig { 
-     @Bean 
-     public OpenAPI springShopOpenAPI() { 
-         return new OpenAPI() 
-                 .info(infoAPI()); 
-     } 
-    
-     
- }
+package tunisie.camp.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI().info(infoAPI());
+    }
+
+    private Info infoAPI() {
+        return new Info()
+                .title("Tunisie Camp API")
+                .description("API documentation for the Tunisie Camp application.")
+                .version("1.0.0")
+                .contact(new Contact()
+                        .name("Gaith")
+                        .email("mohamedgaithb@gmail.com")
+                        .url("https://www.tunisiecamp.com"));
+    }
+}
