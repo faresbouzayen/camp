d9cbd720378188e7bd9f0352173343ebedc3a4bc
BaslyGaith <77758765+BaslyGaith@users.noreply.github.com>
2023-06-16 11:36:48 +0100

    add

--- src/main/java/tunisie/camp/config/ModelMapperConfig.java
+++ src/main/java/tunisie/camp/config/ModelMapperConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ModelMapperConfig {
     @Bean
-    public org.modelmapper.ModelMapper modelMapper(){
+    public org.modelmapper.ModelMapper modelMapper() {
         return new org.modelmapper.ModelMapper();
     }
 }
